f7ec0354f068e8a9c49457276f252f54d974fbaa
Sergio Carralero <sergiocn.dev@gmail.com>
2021-01-19 15:41:45 +0100

    Select symbol refactoring

--- projects/ng-shield-editor/src/lib/components/settings/ng-shield-settings-symbol.component.ts
+++ projects/ng-shield-editor/src/lib/components/settings/ng-shield-settings-symbol.component.ts
@@ -1,4 +1,4 @@
-import {Component, forwardRef, Input} from '@angular/core';
+import {Component, forwardRef, Input, OnInit} from '@angular/core';
 import {ControlValueAccessor, NG_VALUE_ACCESSOR} from '@angular/forms';
 import {NgShieldSettings, NgShieldSettingsSymbol} from '../../ng-shield-settings';
 import {noop} from 'rxjs';
@@ -12,19 +12,19 @@ import {ImageToolService} from '../../services/image-tool.service';
   template: `
     <mat-form-field appearance="fill" class="select-label" *ngIf="settings?.symbol.length > 1">
       <mat-label i18n>Selecciona un símbolo</mat-label>
-      <mat-select [(value)]="selected" [(ngModel)]="settings?.symbol.content" (ngModelChange)="onChange()">
-        <mat-option *ngFor="let symbol of (settings?.symbol || []); index as index" value="{{ index }}">
+      <mat-select [(ngModel)]="selectedSymbol" (ngModelChange)="onChange()">
+        <mat-option *ngFor="let symbol of (settings?.symbol || []); index as index" [value]="symbol">
           <ng-container i18n>Símbolo</ng-container>
           #{{ index + 1 | number }}
         </mat-option>
       </mat-select>
     </mat-form-field>
-
+  
     <div class="symbol-list">
       <div *ngIf="allowNullSelection"
           class="symbol-thumb"
-          [class.active]="settings?.symbol.content === null"
-          (click)="onSymbolSelected(settings.symbol[selected], null)">
+          [class.active]="selectedSymbol.content === null"
+          (click)="onSymbolSelected(selectedSymbol, null)">
         <svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
           <path
             d="m411 255c0-31-8-59-24-84l-216 215c26 17 55 25 85 25 21 0 41-4 60-12 20-8 36-19 50-33 14-14 25-31 33-50 8-19 12-40 12-61z m-285 86l216-216c-26-17-55-26-86-26-28 0-54 7-78 21-24 14-43 33-57 57-13 24-20 50-20 78 0 31 8 59 25 86z m349-86c0 30-5 59-17 86-12 27-27 51-47 70-19 20-43 35-70 47-27 12-55 17-85 17-30 0-58-5-85-17-27-12-51-27-70-47-20-19-35-43-47-70-12-27-17-56-17-86 0-30 5-58 17-85 12-28 27-51 47-71 19-19 43-35 70-46 27-12 55-18 85-18 30 0 58 6 85 18 27 11 51 27 70 46 20 20 35 43 47 71 12 27 17 55 17 85z"></path>
@@ -37,54 +37,54 @@ import {ImageToolService} from '../../services/image-tool.service';
             <path
               d="m165 210l0 55c0 8-3 14-8 20-6 5-12 8-20 8l-55 0c-7 0-14-3-19-8-5-6-8-12-8-20l0-55c0-7 3-14 8-19 5-5 12-8 19-8l55 0c8 0 14 3 20 8 5 5 8 12 8 19z m146 0l0 55c0 8-3 14-8 20-5 5-12 8-20 8l-54 0c-8 0-15-3-20-8-5-6-8-12-8-20l0-55c0-7 3-14 8-19 5-5 12-8 20-8l54 0c8 0 15 3 20 8 5 5 8 12 8 19z m146 0l0 55c0 8-3 14-8 20-5 5-12 8-19 8l-55 0c-8 0-14-3-20-8-5-6-8-12-8-20l0-55c0-7 3-14 8-19 6-5 12-8 20-8l55 0c7 0 14 3 19 8 5 5 8 12 8 19z"></path>
           </svg>
-          <input type="file" accept="image/*" (change)="fileChanged(settings.symbol, $event)" style="display:none"/>
+          <input type="file" accept="image/*" (change)="fileChanged(selectedSymbol, $event)" style="display:none"/>
         </label>
       </div>
 
       <div *ngFor="let availableSymbol of symbolSvc.available"
           class="symbol-thumb"
-          [class.active]="availableSymbol === settings?.symbol.content"
-          (click)="onSymbolSelected(settings.symbol[selected], availableSymbol)">
-        <img [src]="settings?.symbol | fn:getPreview:this:availableSymbol"/>
+          [class.active]="availableSymbol === selectedSymbol.content"
+          (click)="onSymbolSelected(selectedSymbol, availableSymbol)">
+        <img [src]="selectedSymbol | fn:getPreview:this:availableSymbol"/>
       </div>
     </div>
 
     <div class="flex">
       <label>
         <ng-container i18n>Posición horizontal</ng-container>
-        <mat-slider [(ngModel)]="settings?.symbol[selected].x" (input)="settings?.symbol[selected].x = $event.value; onChange()"
-                    [disabled]="settings?.symbol[selected].content === null" [min]="0" [max]="100" [thumbLabel]="true"></mat-slider>
+        <mat-slider [(ngModel)]="selectedSymbol.x" (input)="selectedSymbol.x = $event.value; onChange()"
+                    [disabled]="selectedSymbol.content === null" [min]="0" [max]="100" [thumbLabel]="true"></mat-slider>
       </label>
       <label>
         <ng-container i18n>Posición vertical</ng-container>
-        <mat-slider [(ngModel)]="settings?.symbol[selected].y" (input)="settings?.symbol[selected].y = $event.value; onChange()"
-                    [disabled]="settings?.symbol[selected].content === null" [min]="0" [max]="100" [thumbLabel]="true"></mat-slider>
+        <mat-slider [(ngModel)]="selectedSymbol.y" (input)="selectedSymbol.y = $event.value; onChange()"
+                    [disabled]="selectedSymbol.content === null" [min]="0" [max]="100" [thumbLabel]="true"></mat-slider>
       </label>
     </div>
 
     <div class="flex">
       <label>
         <ng-container i18n>Tamaño</ng-container>
-        <mat-slider [(ngModel)]="settings?.symbol[selected].size" (input)="settings?.symbol[selected].size = $event.value; onChange()"
-                    [disabled]="settings?.symbol[selected].content === null" [min]="1" [max]="200" [thumbLabel]="true"></mat-slider>
+        <mat-slider [(ngModel)]="selectedSymbol.size" (input)="selectedSymbol.size = $event.value; onChange()"
+                    [disabled]="selectedSymbol.content === null" [min]="1" [max]="200" [thumbLabel]="true"></mat-slider>
       </label>
       <label>
         <ng-container i18n>Rotación</ng-container>
-        <mat-slider [(ngModel)]="settings?.symbol[selected].rotation" (input)="settings?.symbol[selected].rotation = $event.value; onChange()"
-                    [disabled]="settings?.symbol[selected].content === null" [min]="-180" [max]="180" [step]="5" [thumbLabel]="true"></mat-slider>
+        <mat-slider [(ngModel)]="selectedSymbol.rotation" (input)="selectedSymbol.rotation = $event.value; onChange()"
+                    [disabled]="selectedSymbol.content === null" [min]="-180" [max]="180" [step]="5" [thumbLabel]="true"></mat-slider>
       </label>
     </div>
 
     <div>
-      <mat-slide-toggle [(ngModel)]="settings?.symbol[selected].trim" (ngModelChange)="onChange()"
-                        [disabled]="settings?.symbol[selected].content === null" i18n>Recortar
+      <mat-slide-toggle [(ngModel)]="selectedSymbol.trim" (ngModelChange)="onChange()"
+                        [disabled]="selectedSymbol.content === null" i18n>Recortar
       </mat-slide-toggle>
     </div>
 
-    <div *ngIf="symbolSvc.isConfigurable(settings?.symbol[selected])" style="margin-top: 10px">
+    <div *ngIf="symbolSvc.isConfigurable(selectedSymbol)" style="margin-top: 10px">
       <label>
         <ng-container i18n>Color</ng-container>
-        <color-picker [(ngModel)]="settings?.symbol[selected].color" (ngModelChange)="onChange()"></color-picker>
+        <color-picker [(ngModel)]="selectedSymbol.color" (ngModelChange)="onChange()"></color-picker>
       </label>
     </div>
 
@@ -160,24 +160,28 @@ import {ImageToolService} from '../../services/image-tool.service';
     }
   ]
 })
-export class NgShieldSettingsSymbolComponent implements ControlValueAccessor {
+export class NgShieldSettingsSymbolComponent implements ControlValueAccessor, OnInit {
   @Input() public allowNullSelection = true;
 
   public settings: NgShieldSettings;
   private _onChangeCallback: (v: any) => void = noop;
-
-  selected = 0;
-
+  
+  public selectedSymbol: NgShieldSettingsSymbol;
+  
   constructor(public symbolSvc: NgShieldSymbolService,
-              private _imageSvc: ImageToolService,
-              private _sanitizer: DomSanitizer) {
+    private _imageSvc: ImageToolService,
+    private _sanitizer: DomSanitizer) {
+  }
+
+  ngOnInit(): void {
+    this.selectedSymbol = this.symbolSvc.defaultSettings;
   }
 
   public onSymbolSelected(symbol: NgShieldSettingsSymbol, content: string) {
     symbol.content = content;
     this.onChange();
   }
-
+    
   public onChange() {
     this.settings = {...this.settings};
     this._onChangeCallback(this.settings);
