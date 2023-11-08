f2135aa9d10e3881d3f2fcc3fd71b8435517c438
Javier Marín <javier@marinros.com>
2023-11-08 19:49:22 +0100

    feat: improve material styles handling

---
+++ projects/ng-shield-editor/src/lib/components/ng-shield-editor-material-styles.component.ts
@@ -0,0 +1,13 @@
+import {ChangeDetectionStrategy, Component, ViewEncapsulation} from '@angular/core';
+
+@Component({
+  selector: 'ng-shield-editor-material-styles',
+  standalone: true,
+  template: '',
+  styleUrl: 'ng-shield-editor-material-styles.component.scss',
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class NgShieldEditorMaterialStylesComponent {
+
+}
--- projects/ng-shield-editor/src/lib/components/ng-shield-editor.component.ts
+++ projects/ng-shield-editor/src/lib/components/ng-shield-editor.component.ts
@@ -4,16 +4,35 @@ import {NgShieldSettings} from '../ng-shield-settings';
 import {imports} from "./imports";
 import {NgShieldEditorPreviewComponent} from "./ng-shield-editor-preview.component";
 import {NgShieldEditorSettingsComponent} from "./ng-shield-editor-settings.component";
+import {NgShieldEditorMaterialStylesComponent} from "./ng-shield-editor-material-styles.component";
 
 @Component({
   selector: 'ng-shield-editor',
   standalone: true,
-  imports: [imports, NgShieldEditorPreviewComponent, NgShieldEditorSettingsComponent],
+  imports: [imports, NgShieldEditorPreviewComponent, NgShieldEditorSettingsComponent, NgShieldEditorMaterialStylesComponent],
   template: `
     <ng-shield-editor-preview *ngIf="showPreview" [settings]="settings"/>
     <ng-shield-editor-settings [(ngModel)]="settings" (ngModelChange)="settingsChange.emit($event)"/>
+    <ng-shield-editor-material-styles style="display: none"/>
   `,
-  styleUrls: ['ng-shield-editor.component.scss']
+  styles: `
+  :host {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: flex-start;
+    gap: 50px;
+  }
+
+  ng-shield-editor-preview {
+    max-width: 400px;
+    flex: 50%;
+  }
+
+  ng-shield-editor-settings {
+    max-width: 100%;
+    flex: 50%;
+  }
+  `
 })
 export class NgShieldEditorComponent {
   @Input() public settings: NgShieldSettings = JSON.parse(JSON.stringify(this._ngShieldSvc.defaultSettings)); // Crear copia para no editar la instancia original
