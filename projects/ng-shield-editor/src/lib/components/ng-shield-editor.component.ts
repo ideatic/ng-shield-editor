8b0c053f963dc999e825a64d2a9c3787da2c016c
Javier Marín <contacto@ideatic.net>
2026-05-08 15:10:09 +0200

    refactor: enforce prettier formatting

--- projects/ng-shield-editor/src/lib/components/ng-shield-editor.component.ts
+++ projects/ng-shield-editor/src/lib/components/ng-shield-editor.component.ts
@@ -1,21 +1,36 @@
-import {ChangeDetectionStrategy, Component, inject, Input, output} from "@angular/core";
-import type {NgShieldSettings} from '../ng-shield-settings';
-import {NgShieldBuilderService} from '../services/ng-shield-builder.service';
-import {imports} from "./imports";
-import {NgShieldEditorMaterialStylesComponent} from "./ng-shield-editor-material-styles.component";
-import {NgShieldEditorPreviewComponent} from "./ng-shield-editor-preview.component";
-import {NgShieldEditorSettingsComponent} from "./ng-shield-editor-settings.component";
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  Input,
+  model,
+  output
+} from '@angular/core';
+import type { NgShieldSettings } from '../ng-shield-settings';
+import { NgShieldBuilderService } from '../services/ng-shield-builder.service';
+import { imports } from './imports';
+import { NgShieldEditorMaterialStylesComponent } from './ng-shield-editor-material-styles.component';
+import { NgShieldEditorPreviewComponent } from './ng-shield-editor-preview.component';
+import { NgShieldEditorSettingsComponent } from './ng-shield-editor-settings.component';
 
 @Component({
   selector: 'ng-shield-editor',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [imports, NgShieldEditorPreviewComponent, NgShieldEditorSettingsComponent, NgShieldEditorMaterialStylesComponent],
+  imports: [
+    imports,
+    NgShieldEditorPreviewComponent,
+    NgShieldEditorSettingsComponent,
+    NgShieldEditorMaterialStylesComponent
+  ],
   template: `
-    @if (showPreview) {
-      <ng-shield-editor-preview [settings]="settings"/>
+    @if (showPreview()) {
+      <ng-shield-editor-preview [settings]="settings" />
     }
-    <ng-shield-editor-settings [(ngModel)]="settings" (ngModelChange)="settingsChange.emit($event)"/>
-    <ng-shield-editor-material-styles style="display: none"/>
+    <ng-shield-editor-settings
+      [(ngModel)]="settings"
+      (ngModelChange)="settingsChange.emit($event)"
+    />
+    <ng-shield-editor-material-styles style="display: none" />
   `,
   styles: `
     :host {
@@ -26,13 +41,13 @@ import {NgShieldEditorSettingsComponent} from "./ng-shield-editor-settings.compo
     }
 
     ng-shield-editor-preview {
-      max-width: 400px;
       flex: 50%;
+      max-width: 400px;
     }
 
     ng-shield-editor-settings {
-      max-width: 100%;
       flex: 50%;
+      max-width: 100%;
     }
   `
 })
@@ -41,15 +56,22 @@ export class NgShieldEditorComponent {
   private _ngShieldSvc = inject(NgShieldBuilderService);
 
   // Bindings
-  // TODO: Skipped for migration because:
-  //  Your application code writes to the input. This prevents migration.
-  @Input() public settings: NgShieldSettings = JSON.parse(JSON.stringify(this._ngShieldSvc.defaultSettings)); // Crear copia para no editar la instancia original
-  // TODO: Skipped for migration because:
-  //  Your application code writes to the input. This prevents migration.
-  @Input() public showPreview = true;
+  @Input() public settings: NgShieldSettings = JSON.parse(
+    JSON.stringify(this._ngShieldSvc.defaultSettings)
+  ); // Crear copia para no editar la instancia original
+  public readonly showPreview = model(true);
   public readonly settingsChange = output<NgShieldSettings>();
 
-  public render(width: number, height: number, type = 'image/png'): Promise<string> {
-    return this._ngShieldSvc.renderBase64Image(this.settings, width, height, type);
+  public render(
+    width: number,
+    height: number,
+    type = 'image/png'
+  ): Promise<string> {
+    return this._ngShieldSvc.renderBase64Image(
+      this.settings,
+      width,
+      height,
+      type
+    );
   }
 }
